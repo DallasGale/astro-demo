4c767150a7f4a6861c6c0e55aada6f6e9445b371
Dallas Gale <dallasgale.digital@gmail.com>
2021-08-10 22:36:59 +1000

    main

---
+++ src/components/react/header/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import nav_items from "./navigation.json";
+
+const Header = (logo) => {
+  console.log(logo);
+  const [navState, setNavState] = useState("default");
+
+  return (
+    <header className="header">
+      <h1 className="header__logo">
+        <img src={logo.logo} alt="The New Yorker" />
+      </h1>
+      <nav className="nav" data-state={`default`}>
+        <ul className="nav__ul">
+          {nav_items.map((data) => {
+            console.log({ data });
+            return (
+              <li key={data.label} className="nav__li" data-color={data.color}>
+                <a className="nav__link" href={data.slug}>
+                  {data.label}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
