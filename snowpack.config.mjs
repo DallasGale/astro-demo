baf833274383822fbc2007ac90d2f483c3f2a2e0
Dallas Gale <dallasgale.digital@gmail.com>
2021-08-13 09:07:38 +1000

    updates to layout

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -2,4 +2,5 @@ export default {
   env: {
     DATO_API_TOKEN: "8062d0df4df4609f57f01a4e4071e4",
   },
+  plugins: [["@snowpack/plugin-typescript"]],
 };
--- src/components/react/post-tile/index.tsx
+++ src/components/react/post-tile/index.tsx
@@ -1,12 +1,86 @@
 import React from "react";
 
-const PostTile = ({ title, excerpt }) => {
-  return (
-    <div className="post-tile">
-      {title}
-      {excerpt}
-    </div>
-  );
+interface Props {
+  image: string;
+  category: string;
+  title: string;
+  excerpt: string;
+  tileType: "feature" | "mini" | "strip";
+  isFeature: boolean;
+  isMini: boolean;
+}
+
+const PostTile = ({
+  image,
+  category,
+  title,
+  excerpt,
+  author,
+  tileType,
+}): Props => {
+  if (tileType === "feature") {
+    return (
+      <div className="post-tile post-tile--feature">
+        <picture className="post-tile--picture">
+          <source srcSet={image} />
+          <img src={image} alt="Buzz" />
+        </picture>
+
+        <div className="post-tile--copy">
+          <small className="post-tile--category">{category}</small>
+          <h2 className="post-tile--title">{title}</h2>
+          <p className="post-tile--excerpt">{excerpt}</p>
+          <p className="post-tile--author">{author}</p>
+        </div>
+      </div>
+    );
+  } else if (tileType === "mini") {
+    return (
+      <div className="post-tile post-tile--mini">
+        <picture className="post-tile--picture">
+          <source srcSet={image} />
+          <img src={image} alt="Buzz" />
+        </picture>
+
+        <div className="post-tile--copy">
+          <small className="post-tile--category">{category}</small>
+          <h2 className="post-tile--title">{title}</h2>
+        </div>
+      </div>
+    );
+  } else if (tileType === "strip") {
+    return (
+      <div className="post-tile post-tile--strip">
+        <picture className="post-tile--picture">
+          <source srcSet={image} />
+          <img src={image} alt="Buzz" />
+        </picture>
+
+        <div className="post-tile--copy">
+          <small className="post-tile--category">{category}</small>
+          <h2 className="post-tile--title">{title}</h2>
+          <p className="post-tile--excerpt">{excerpt}</p>
+          <p className="post-tile--author">{author}</p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="post-tile post-tile--regular">
+        <picture className="post-tile--picture">
+          <source srcSet={image} />
+          <img src={image} alt="Buzz" />
+        </picture>
+
+        <div className="post-tile--copy">
+          <small className="post-tile--category">{category}</small>
+          <h2 className="post-tile--title">{title}</h2>
+          <p className="post-tile--excerpt">{excerpt}</p>
+          <p className="post-tile--author">{author}</p>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default PostTile;
